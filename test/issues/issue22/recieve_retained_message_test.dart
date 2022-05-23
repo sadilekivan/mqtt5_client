81e56561634bb8e31c887c0abce8eec7dd1359a1
Ivan Sadílek <sadilekivan@gmail.com>
2022-05-23 12:58:21 +0200

    testing with multiple brokers, future timeout on recieve

--- test/issues/issue22/recieve_retained_message_test.dart
+++ test/issues/issue22/recieve_retained_message_test.dart
@@ -5,8 +5,8 @@ import 'package:typed_data/typed_buffers.dart';
 import 'package:test/test.dart';
 
 Future<int> main() async {
-  recieveRetainedMessageTest(int retainDelay) async {
-    final client = MqttServerClient("test.mosquitto.org", "");
+  recieveRetainedMessageTest(String broker_url, int retainDelay) async {
+    final client = MqttServerClient(broker_url, "");
     
     String topic = "iWantToBeRetained";
     var sendData = Uint8Buffer();
@@ -21,29 +21,34 @@ Future<int> main() async {
     await MqttUtilities.asyncSleep(retainDelay);
 
     client.subscribe(topic, MqttQos.exactlyOnce);
-    client.updates.listen((List<MqttReceivedMessage<MqttMessage>> c) {
-      final data = (c[0].payload as MqttPublishMessage).payload.message!;
-      print("Recieved Message: ${data} at ${c[0].topic}");
-      if (c[0].topic == topic) {
-        recvData = data;
-      }
-    });
-
-    // Give some time to the broker for a response
-    await MqttUtilities.asyncSleep(1);
-    if (recvData == null) {
-      print("Nothing was recieved!");
-    }
     
+    Future recieveRetained() {
+      var completer = Completer();
+      client.updates.listen((List<MqttReceivedMessage<MqttMessage>> c) {
+        final data = (c[0].payload as MqttPublishMessage).payload.message!;
+        print("Recieved Message: ${data} at ${c[0].topic}");
+        if (c[0].topic == topic) {
+          completer.complete(data);
+        }
+      });
+      return completer.future;
+    }
+
+    recvData = await recieveRetained().timeout(Duration(seconds: 5), onTimeout: () => print("Broker timeout, nothing was recieved!"));
+
     expect(recvData, sendData);
 
     client.unsubscribeStringTopic(topic);
     client.disconnect();
   }
 
-  test("Get a retained message right after its published", () => recieveRetainedMessageTest(0));
+  test("[test.mosquitto.org] right after its published", () => recieveRetainedMessageTest("test.mosquitto.org", 0));
 
-  test("Get a retained message after a delay", () => recieveRetainedMessageTest(1));
+  test("[test.mosquitto.org] after a delay", () => recieveRetainedMessageTest("test.mosquitto.org", 1));
+
+  test("[broker.hivemq.com] right after its published", () => recieveRetainedMessageTest("broker.hivemq.com", 0));
+
+  test("[broker.hivemq.com] after a delay", () => recieveRetainedMessageTest("broker.hivemq.com", 1));
 
   return 0;
 }
