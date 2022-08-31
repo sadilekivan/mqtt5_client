c99c16cfdb417ab7cb8d08fd96194a21b195cadc
Ivan Sadílek <sadilekivan@gmail.com>
2022-08-31 11:33:16 +0200

    testing different brokers

--- test/issues/issue22/recieve_retained_message_test.dart
+++ test/issues/issue22/recieve_retained_message_test.dart
@@ -5,9 +5,11 @@ import 'package:typed_data/typed_buffers.dart';
 import 'package:test/test.dart';
 
 Future<int> main() async {
-  recieveRetainedMessageTest(String broker_url, int retainDelay) async {
+  recieveRetainedMessageTest(String broker_url, int port, bool useWs, int retainDelay) async {
     final client = MqttServerClient(broker_url, "");
-    
+    client.useWebSocket = useWs;
+    client.port = port;
+
     String topic = "iWantToBeRetained";
     var sendData = Uint8Buffer();
     sendData.add(255); //Super unique data
@@ -16,12 +18,12 @@ Future<int> main() async {
     await client.connect();
     print("Publishing Message: $sendData at $topic");
     client.publishMessage(topic, MqttQos.exactlyOnce, sendData, retain: true);
-    
+
     print("Waiting for $retainDelay seconds before subscribing");
     await MqttUtilities.asyncSleep(retainDelay);
 
     client.subscribe(topic, MqttQos.exactlyOnce);
-    
+
     Future recieveRetained() {
       var completer = Completer();
       client.updates.listen((List<MqttReceivedMessage<MqttMessage>> c) {
@@ -34,21 +36,33 @@ Future<int> main() async {
       return completer.future;
     }
 
-    recvData = await recieveRetained().timeout(Duration(seconds: 5), onTimeout: () => print("Broker timeout, nothing was recieved!"));
+    recvData = await recieveRetained().timeout(Duration(seconds: 60), onTimeout: () => print("Broker timeout, nothing was recieved!"));
 
     expect(recvData, sendData);
 
     client.unsubscribeStringTopic(topic);
     client.disconnect();
   }
+ 
+  //WS
+  test("[broker.emqx.io] websocket right after its published", () => recieveRetainedMessageTest("ws://broker.emqx.io/mqtt", 8083, true, 0));
 
-  test("[test.mosquitto.org] right after its published", () => recieveRetainedMessageTest("test.mosquitto.org", 0));
+  test("[broker.emqx.io] websocket after a delay", () => recieveRetainedMessageTest("ws://broker.emqx.io/mqtt", 8083, true, 1));
 
-  test("[test.mosquitto.org] after a delay", () => recieveRetainedMessageTest("test.mosquitto.org", 1));
+  test("[broker.hivemq.com] websocket right after its published", () => recieveRetainedMessageTest("ws://broker.hivemq.com/mqtt", 8000, true, 0));
 
-  test("[broker.hivemq.com] right after its published", () => recieveRetainedMessageTest("broker.hivemq.com", 0));
+  test("[broker.hivemq.com] websocket after a delay", () => recieveRetainedMessageTest("ws://broker.hivemq.com/mqtt", 8000, true, 1));
 
-  test("[broker.hivemq.com] after a delay", () => recieveRetainedMessageTest("broker.hivemq.com", 1));
+  //TCP
+
+  test("[broker.emqx.io] tcp right after its published", () => recieveRetainedMessageTest("broker.emqx.io", 1883, false, 0));
+
+  test("[broker.emqx.io] tcp after a delay", () => recieveRetainedMessageTest("broker.emqx.io", 1883, false, 1));
+
+  test("[broker.hivemq.com] tcp right after its published", () => recieveRetainedMessageTest("broker.hivemq.com", 1883, false, 0));
+
+  test("[broker.hivemq.com] tcp after a delay", () => recieveRetainedMessageTest("broker.hivemq.com", 1883, false, 1));
+
 
   return 0;
 }
